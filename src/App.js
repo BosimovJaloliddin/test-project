5aa96a1779ea6396473f2a47b23e63eaf5ed73b6
Jaloliddin Bosimov <bosimovjaloliddin@gmail.com>
2024-05-09 16:09:15 +0500

    ..

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
-      <div className="container">Header</div>
+      <Header />
       <div
         style={{
           display: "flex",
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -5,14 +5,56 @@ const Navbar = () => {
   return (
     <div className="container">
       <div className="navbar-one">
-        <div className="navbar-one_item">1</div>
-        <div className="navbar-one_item">2</div>
+        <div className="navbar-one_city">
+          <img src={require("../../assets/imgs/map.png")} alt="map" />
+          <select name="city" id="city">
+            <option value="1">Ташкент</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+          </select>
+        </div>
+        <div className="navbar-one_lang">
+          <select name="city" id="city">
+            <img src={require("../../assets/imgs/map.png")} alt="map" />
+            <option value="1">Ташкент</option>
+            <option value="2">
+              <img src={require("../../assets/imgs/map.png")} alt="map" />2
+            </option>
+            <option value="3">
+              <img src={require("../../assets/imgs/map.png")} alt="map" />3
+            </option>
+            <option value="4">
+              <img src={require("../../assets/imgs/map.png")} alt="map" />4
+            </option>
+          </select>
+        </div>
       </div>
       <div className="navbar-two">
-        <div className="navbar-two_logo">Logo</div>
-        <div className="navbar-two_catalog">Catalog</div>
-        <div className="navbar-two_nav">Nav</div>
-        <div className="navbar-two_login">Login</div>
+        <div className="navbar-two_logo">
+          <img src={require("../../assets/imgs/Logo.png")} alt="Logo" />
+          <span>
+            Консультация <br /> от врачей
+          </span>
+        </div>
+        <div className="navbar-two_catalog">
+          <img src={require("../../assets/imgs/apps.png")} alt="apps" />
+          Каталог услуг
+        </div>
+        <ul className="navbar-two_nav">
+          <li>
+            <a href="#/">О проекте</a>
+          </li>
+          <li>
+            <a href="#/">Партнерство</a>
+          </li>
+          <li>
+            <a href="#/">Контакты</a>
+          </li>
+        </ul>
+        <div className="navbar-two_login">
+          <a href="#/">Вход</a>/<a href="#/">Регистрация</a>
+        </div>
       </div>
     </div>
   );
