ee63db9f6907475ea0a8b50f650acec7a8595aa8
Jaloliddin Bosimov <bosimovjaloliddin@gmail.com>
2024-05-09 17:16:52 +0500

    add header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,7 +2,59 @@ import React from "react";
 import "./style.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="container">
+      <div className="header-items">
+        <img
+          className="header-vektor_img"
+          src={require("../../assets/imgs/header-vektor.png")}
+          alt="vektor"
+        />
+        <p className="header-item header-info">Консультация от врачей</p>
+        <h1 className="header-item header-title">
+          Получайте бесплатную консультацию от <br /> лучших специалистов
+        </h1>
+        <div className="header-item header-menu_items">
+          <div className="header-menu_item">
+            <img src={require("../../assets/imgs/star.png")} alt="star" />
+            <span>Лучшие консультанты</span>
+          </div>
+          <div className="header-menu_item">
+            <img
+              src={require("../../assets/imgs/question-circle.png")}
+              alt="star"
+            />
+            <span>Задать вопрос</span>
+          </div>
+          <div className="header-menu_item">
+            <img
+              src={require("../../assets/imgs/files-landscapes-alt.png")}
+              alt="star"
+            />
+            <span>Популярные вопросы</span>
+          </div>
+        </div>
+        <div className="header-item header-result_items">
+          <div className="header-result_item">
+            <span>+1 000</span>
+            <p>Врачей различных специализаций</p>
+          </div>
+          <div className="header-result_item">
+            <span>24/7</span>
+            <p>Доступ к медицинской помощи</p>
+          </div>
+          <div className="header-result_item">
+            <span>+95%</span>
+            <p>Положительных отзывов</p>
+          </div>
+          <div className="header-result_item">
+            <span>+20 000</span>
+            <p>Анкет пользователей</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
