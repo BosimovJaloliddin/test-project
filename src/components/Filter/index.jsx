0f7a2d4dd8e4418dab63c21fac953ccd1983f871
Jaloliddin Bosimov <bosimovjaloliddin@gmail.com>
2024-05-10 09:48:23 +0500

    add filter section

--- src/components/Filter/index.jsx
+++ src/components/Filter/index.jsx
@@ -12,14 +12,38 @@ const Filter = () => {
             <span>Сбросить</span>
           </div>
         </div>
-        <div className="filter-item">
-          <input type="text" />
-          <img src="" alt="" />
+        <div className="filter-item filter-item_search">
+          <input type="text" placeholder="Поиск направления" />
+          <img src={require("../../assets/imgs/search.png")} alt="search" />
         </div>
-        <div className="filter-item">1</div>
-        <div className="filter-item">1</div>
-        <div className="filter-item">1</div>
-        <div className="filter-item">1</div>
+        <div className="filter-item">
+          <div className="filter-item_checkbox">
+            <input type="checkbox" id="allergolog" />
+            <label htmlFor="allergolog">Аллерголог</label>
+          </div>
+          <p className="filter-checkbox_nums">(12 009)</p>
+        </div>
+        <div className="filter-item">
+          <div className="filter-item_checkbox">
+            <input type="checkbox" id="allergolog" />
+            <label htmlFor="allergolog">Аллерголог</label>
+          </div>
+          <p className="filter-checkbox_nums">(12 009)</p>
+        </div>
+        <div className="filter-item">
+          <div className="filter-item_checkbox">
+            <input type="checkbox" id="allergolog" />
+            <label htmlFor="allergolog">Аллерголог</label>
+          </div>
+          <p className="filter-checkbox_nums">(12 009)</p>
+        </div>
+        <div className="filter-item">
+          <div className="filter-item_checkbox">
+            <input type="checkbox" id="allergolog" />
+            <label htmlFor="allergolog">Аллерголог</label>
+          </div>
+          <p className="filter-checkbox_nums">(12 009)</p>
+        </div>
       </div>
     </section>
   );
